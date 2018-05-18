5a4758b77ad3ac967f6ece3352a4a43f8b543c42
pedromassango <pedromassango.developer@gmail.com>
2018-05-19 00:43:09 +0100

    Setup

---
+++ app/src/main/java/com/pedromassango/jobschedulersample/MainActivity.java
@@ -0,0 +1,62 @@
+package com.pedromassango.jobschedulersample;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.JobIntentService;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ComponentName serviceComponentName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        serviceComponentName = new ComponentName(this, MyJobService.class);
+    }
+
+    // To stop job
+    public void onStartJob(View v){
+        JobInfo.Builder builder = new JobInfo.Builder(0, serviceComponentName);
+        builder.setOverrideDeadline( 1000); // delay time after scheduled the job
+
+        // Start the job
+        JobScheduler scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        scheduler.schedule(builder.build());
+
+        showStatus("Job running");
+    }
+
+    // To stop the job
+    public void onStopJob(View v){
+        JobScheduler scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        //scheduler.cancel(0);
+        List<JobInfo> jobs = scheduler.getAllPendingJobs();
+
+        if(jobs.isEmpty()){
+            showStatus("No Job to cancel");
+        }else{
+            int id = jobs.get(0).getId();
+
+            scheduler.cancel(id);
+            showStatus("Job stopped");
+        }
+    }
+
+    private void showStatus(String state){
+        TextView textView = findViewById(R.id.tv_job_state);
+        textView.setText( state);
+    }
+}
---
+++ app/src/main/java/com/pedromassango/jobschedulersample/MyJobService.java
@@ -0,0 +1,81 @@
+package com.pedromassango.jobschedulersample;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Pedro Massango on 5/16/18.
+ */
+public class MyJobService extends JobService {
+
+    // JobService thread
+    private JobThread jobThread;
+
+    private void log(String msg) {
+        Log.e("MyJobService", msg);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        log("onCreate();");
+
+        jobThread = new JobThread();
+        jobThread.start(); // start the thread when Service is created
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        log("onStartCommand();");
+
+        // since onStartCommand is called only if the thread is already running
+        // we just ignore this method, since we already started the thread in onCreate()
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        log("onStartJob();");
+
+        // we just ignore this method,
+        // since we already started the thread in onCreate()
+        return true; // return true to let the service do the job
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        log("onStopJob();");
+
+        // change the thread state to cancel the execution
+        jobThread.stopThread = true;
+        return false;
+    }
+
+    class JobThread extends Thread{
+
+        boolean stopThread = false;
+
+        @Override
+        public void run() {
+            int count = 0;
+
+            // TODO: this will block the UI because JobService run on mainThread.
+            while (count != 100 && !stopThread){
+                count++;
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    stopSelf();
+                    log("Thread interrupted");
+                }
+
+                log("count: " +count);
+            }
+        }
+    }
+
+}
